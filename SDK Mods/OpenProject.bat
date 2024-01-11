6ecf4661a04e087ab0a0be972352d65716607539
thakyZ <950594+thakyZ@users.noreply.github.com>
2024-01-11 07:28:32 -0800

    update: open project scripts

--- SDK Mods/OpenProject.bat
+++ SDK Mods/OpenProject.bat
@@ -1,2 +1,18 @@
 @echo off
+
+SET mypath=%~dp0
+where /q pwsh
+SET exitcode=1
+IF ERRORLEVEL 1 (
+    powershell -ExecutionPolicy Unrestricted -File " & ""%mypath%\Open-Project.ps1" ""%*"" "
+    SET exitcode=%ERRORLEVEL%
+) ELSE (
+    pwsh -ExecutionPolicy Unrestricted -File " & ""%mypath%\Open-Project.ps1"" ""%*"" "
+    SET exitcode=%ERRORLEVEL%
+)
+
+IF "%exitcode%"=="1" (
+  EXIT /B
+)
+
 powershell -executionpolicy unrestricted -File OpenProject.ps1 %*
---
+++ SDK Mods/open_project.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+
+echo "Script Directory = \"${SCRIPT_DIR}\"";
+
+PWSH=$(which "pwsh")
+
+exitcode=1
+
+if [ -z "${PWSH}" ]; then
+  powershell -noprofile -c "& \"${SCRIPT_DIR}\.gpg-pinentry-loopback.ps1\" \"$*\""
+  exitcode=$?
+else
+  "${PWSH}" -noprofile -c "& \"${SCRIPT_DIR}\.gpg-pinentry-loopback.ps1\" \"$*\""
+  exitcode=$?
+fi
+
+exit $exitcode
