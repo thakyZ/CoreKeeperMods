185df5a7af8f43b6dbb9fd00f2a266bdcbe78350
thakyZ <950594+thakyZ@users.noreply.github.com>
2024-10-29 07:46:31 -0700

    update: refactored nbnAccessories folder

---
+++ SDK Mods/Assets/Mods/nbnAccessories/Scripts/AccessoriesMod.cs
@@ -0,0 +1,36 @@
+#nullable enable
+using Object = UnityEngine.Object;
+using PugMod;
+
+// ReSharper disable once CheckNamespace
+namespace NekoBoiNick.Accessories {
+  public class AccessoriesMod : IMod {
+    public const string VERSION = "1.0.0";
+    public const string NAME = "nbnAccessories";
+    public const string AUTHOR = "Neko Boi Nick";
+
+    public void EarlyInit() {
+      // throw new System.NotImplementedException();
+    }
+
+    public void Init() {
+      // throw new System.NotImplementedException();
+    }
+
+    public void Shutdown() {
+      // throw new System.NotImplementedException();
+    }
+
+    public void ModObjectLoaded(Object obj) {
+      // throw new System.NotImplementedException();
+    }
+
+    public bool CanBeUnloaded() {
+      return true;
+    }
+
+    public void Update() {
+      // throw new System.NotImplementedException();
+    }
+  }
+}
